87e7fcd16d3253df2243b4026b921694dad7a432
aldimahardiansyah <aldimdiansyah@gmail.com>
2023-02-27 20:03:47 +0700

    tes form belanja

--- coba-2/proses_nilai.php
+++ coba-2/proses_nilai.php
@@ -1,13 +1,17 @@
 <?php
-        $submit = $_GET['submit'];
-        $name = $_GET['name'];
-        $matkul = $_GET['matkul'];
-        $uts = $_GET['uts'];
-        $uas = $_GET['uas'];
-        $task = $_GET['task'];
+$submit = $_GET['submit'];
+$name = $_GET['name'];
+$matkul = $_GET['matkul'];
+$uts = $_GET['uts'];
+$uas = $_GET['uas'];
+$task = $_GET['task'];
 
-        echo "Nama: $name <br>";
-        echo "Mata Kuliah: $matkul <br>";
-        echo "Nilai UTS: $uts <br>";
-        echo "Nilai UAS: $uas <br>";
-        echo "Nilai Tugas Praktikum: $task";
+if (isset($submit)) {
+    echo "Nama: $name <br>";
+    echo "Mata Kuliah: $matkul <br>";
+    echo "Nilai UTS: $uts <br>";
+    echo "Nilai UAS: $uas <br>";
+    echo "Nilai Tugas Praktikum: $task";
+} else {
+    echo "Tidak ada data!";
+}
