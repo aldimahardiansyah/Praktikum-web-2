b293923cdc27499213d6e21ec668224d6defd59d
aldimahardiansyah <aldimdiansyah@gmail.com>
2023-04-05 15:31:16 +0700

    add task

---
+++ pertemuan7/Motor.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Tugas:
+ * 1. Buat class Motor
+ * 2. Isi atributnya: merk, bensin, topSpeed, cc
+ * 3. Dibuatkan constructornya
+ * 4. Buat method `jalan` yang menerima parameter `jarak`.
+ * Fungsi method berisi: mengurangi bensin sesuai jarak. 
+ * Misal: 1liter untuk 30km
+ */
